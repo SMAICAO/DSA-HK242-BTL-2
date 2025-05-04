b8858d5a568aa04cb72851a1bcc991e9edcfc8c7
Trương Phước Minh Hoàng <153970350+SMAICAO@users.noreply.github.com>
2025-05-04 17:08:59 +0700

    Dev Inventory Compressor

--- include/app/inventory_compressor.h
+++ include/app/inventory_compressor.h
@@ -159,33 +159,75 @@ std::string HuffmanTree<treeOrder>::decode(const std::string &huffmanCode)
     DLinkedList<char> symbols = table.keys();
     DLinkedList<std::string> codes = table.values();
 
-    int index = codes.indexOf(huffmanCode);
-    // Code found
-    if (index != -1) {
-        return symbols.get(index);
-    
-    // Code not found
-    } else {
-        return std::string();
+    string decodedStr = "";
+    for (int i = 0; i < huffmanCode.length(); ++i) {
+        std::string code = huffmanCode.substr(i, 1);
+        for (int j = i; j < huffmanCode.length(); ++j) {
+            if (codes.contains(code)) {
+                int index = codes.indexOf(code);
+                decodedStr += symbols.get(index);
+                i += code.length() - 1;
+                break;
+            } else {
+                code += huffmanCode[j];
+            }
+        }
     }
+    return decodedStr;
 }
 
 template <int treeOrder>
 InventoryCompressor<treeOrder>::InventoryCompressor(InventoryManager *manager)
 {
     //TODO
+    invManager = manager;
+    huffmanTable = new xMap<char, std::string>([](char symbol, int capacity)->int {
+        return 0;
+    });
+    tree = new HuffmanTree<treeOrder>();
 }
 
 template <int treeOrder>
 InventoryCompressor<treeOrder>::~InventoryCompressor()
 {
     //TODO
+    delete huffmanTable;
+    delete invManager;
+    delete tree;
 }
 
 template <int treeOrder>
 void InventoryCompressor<treeOrder>::buildHuffman()
 {
     //TODO
+    // (a) Traverse all products to build a frequency table for each character in the string
+    // representation of a product using productToString.
+    XArrayList<pair<char, int>> symbolsFreqs(0, [](pair<char, int> a, pair<char, int> b)->bool {
+        return a.first == b.first;
+    }, 256);
+
+    // Get product strings
+    std::string productStr = "";
+    for (int i = 0; i < invManager->size(); ++i) {
+        List1D<InventoryAttribute> attributes = invManager->getProductAttributes(i);
+        std::string name = invManager->getProductName(i);
+        productStr += productToString(attributes, name);
+    }
+
+    // Count frequencies of each character in the product string
+    for (int i = 0; i < productStr.length(); ++i) {
+        char ch = productStr[i];
+        int freq = 1;
+        if (symbolsFreqs.contains(make_pair(ch, freq))) {
+            symbolsFreqs.get(symbolsFreqs.indexOf(make_pair(ch, freq))).second++;
+        } else {
+            symbolsFreqs.add(make_pair(ch, freq));
+        }
+    }
+
+    // (b) Create the huffmanTable by building a Huffman tree using the frequency table.
+    tree->build(symbolsFreqs);
+    tree->generateCodes(*huffmanTable);
 }
 
 template <int treeOrder>
@@ -200,19 +242,69 @@ template <int treeOrder>
 std::string InventoryCompressor<treeOrder>::productToString(const List1D<InventoryAttribute> &attributes, const std::string &name)
 {
     //TODO
-    return std::string();
+    stringstream ss;
+
+    ss << name << ":";
+    for (int i = 0; i < attributes.size(); ++i) {
+        if (i > 0) {
+            ss << ", ";
+        }
+        
+        ss << "("
+            << attribute.get(i).name
+            << ": "
+            << attribute.get(i).value
+            << ")";
+    }
+
+    return ss.str();
 }
 
 template <int treeOrder>
 std::string InventoryCompressor<treeOrder>::encodeHuffman(const List1D<InventoryAttribute> &attributes, const std::string &name)
 {
     //TODO
-    return std::string();
+    // Convert product data to a string using productToString,
+    // then generate the code using huffmanTable.
+    std::string productStr = productToString(attributes, name);
+    std::string huffmanCode = "";
+    for (int i = 0; i < productStr.length(); ++i) {
+        char ch = productStr[i];
+        huffmanCode += huffmanTable->get(ch);
+    }
+    return huffmanCode;
 }
 
 template <int treeOrder>
 std::string InventoryCompressor<treeOrder>::decodeHuffman(const std::string &huffmanCode, List1D<InventoryAttribute> &attributesOutput, std::string &nameOutput)
 {
     //TODO
-    return std::string();
+    // (a) Decode the string using the Huffman tree.
+    std::string productString = tree->decode(huffmanCode);
+
+    // (b) Parse the result string to extract the product name and attribute list. The
+    // expected format is: "product name: (attribute1: value1), (attribute2: value2),
+    // ...".
+    std::string productName = productString.substr(0, productString.find(":"));
+    std::string attributesStr = productString.substr(productString.find(":") + 1);
+    List1D<InventoryAttribute> attributeList;
+    while (attributesStr.length() > 0) {
+        int start = attributesStr.find("(");
+        int end = attributesStr.find(")");
+        if (start == std::string::npos || end == std::string::npos) {
+            break;
+        }
+        std::string attributeStr = attributesStr.substr(start + 1, end - start - 1);
+        std::string name = attributeStr.substr(0, attributeStr.find(":"));
+        double value = std::stod(attributeStr.substr(attributeStr.find(":") + 1));
+        InventoryAttribute attribute(name, value);
+        attributeList.add(attribute);
+        attributesStr = attributesStr.substr(end + 1);
+    }
+
+    // (c) The method returns the decoded string. At the same time, the product name and
+    // attribute list are assigned to nameOutput and attributesOutput, respectively.
+    nameOutput = productName;
+    attributesOutput = attributeList;
+    return productString;
 }
