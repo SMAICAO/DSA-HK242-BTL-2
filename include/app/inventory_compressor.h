c4fa267d50549b5f10eb4a61e3370736000feb1f
SMAICAO <aleximination@gmail.com>
2025-05-04 09:42:42 +0700

    Dev Huffman tree

--- include/app/inventory_compressor.h
+++ include/app/inventory_compressor.h
@@ -20,8 +20,8 @@ public:
         int freq;
         XArrayList<HuffmanNode*> children;
 
-        HuffmanNode(char s, int f); //Leaf node
-        HuffmanNode(int f, const  XArrayList<HuffmanNode*>& childs); //Internal node
+        HuffmanNode(char s, int f) : symbol(s), freq(f) {}; //Leaf node
+        HuffmanNode(int f, const  XArrayList<HuffmanNode*>& childs) : freq(f), children(childs) {}; //Internal node
     };
 
     HuffmanTree();
@@ -74,31 +74,100 @@ void HuffmanTree<treeOrder>::build(XArrayList<pair<char, int>>& symbolsFreqs)
 {
     //TODO
     //(a) Create a heap from the given list.
-    Heap<HuffmanNode*> heap;
+    Heap<HuffmanNode*> heap([](HuffmanNode* a, HuffmanNode* b) -> int {
+        if (a->freq < b->freq) return -1;
+        else if (a->freq > b->freq) return 1;
+        else if (a->symbol < b->symbol) return -1;
+        else if (a->symbol > b->symbol) return 1;
+        else return 0;
+    }, 0);
+
     for (auto it = symbolsFreqs.begin(); it != symbolsFreqs.end(); ++it) {
-        HuffmanNode* node = new HuffmanNode(it->first, it->second);
+        HuffmanNode* node = new HuffmanNode((*it)->first, (*it)->second);
         heap.push(node);
+    }
+
+    if ((symbolsFreqs.size() - 1) % (treeOrder - 1) != 0) {
+        for (int i = 0; i < (treeOrder - 1) - ((symbolsFreqs.size() - 1) % (treeOrder - 1)); ++i) {
+            HuffmanNode* node = new HuffmanNode('\0', 0);
+            heap.push(node);
+        }
     }
 
     //(b) While the heap has more than one node, select up to treeOrder nodes with the
     //lowest frequencies.
-    //(c) Compute the total frequency and group selected nodes into a list.
-    //(d) Create a new internal node with the computed frequency and list of children.
-    //(e) Push the new internal node into the heap.
-    //(f) After the loop, the last remaining node becomes the root of the tree.
+    while (heap.size() > 1) {
+        //(c) Compute the total frequency and group selected nodes into a list.
+        XArrayList<HuffmanNode*> children;
+        int totalFreq = 0;
+        for (int i = 0; i < treeOrder && heap.size() > 0; ++i) {
+            HuffmanNode* child = heap.pop();
+            children.add(child);
+            totalFreq += child->freq;
+        }
+
+        //(d) Create a new internal node with the computed frequency and list of children.
+        HuffmanNode* parent = new HuffmanNode(totalFreq, children);
+
+        //(e) Push the new internal node into the heap.
+        heap.push(parent);
+    }
+
+    //(f) After the loop, the last remaining node becomes the root of the tree
+    root = heap.pop();
 }
 
 template <int treeOrder>
 void HuffmanTree<treeOrder>::generateCodes(xMap<char, std::string> &table)
 {
     //TODO
+    // Base 16 for encoding
+    char base16[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    
+    // DFS to generate the codes
+    std::string code = "";
+    XArrayList<pair<HuffmanNode*, std::string>> stack;
+    stack.push(make_pair(root, code));
+
+    while (!stack.empty()) {
+        auto nodePair = stack.pop();
+        HuffmanNode* node = nodePair.first;
+        std::string code = nodePair.second;
+
+        // If node is leaf
+        if (node->children.empty()) {
+            table.put(node->symbol, code);
+        
+        // If node is parent
+        } else {
+            for (int i = 0; i < node->children.size(); ++i) {
+                stack.push(make_pair(node->children.get(i), code + base16[i]));
+            }
+        }
+    }
 }
 
 template <int treeOrder>
 std::string HuffmanTree<treeOrder>::decode(const std::string &huffmanCode)
 {
     //TODO
-    return std::string();
+    // Get codes
+    xMap<char, std::string> &table;
+    generateCodes(table);
+
+    // Find the symbol corresponding to the given huffman code
+    DLinkedList<char> symbols = table.keys();
+    DLinkedList<std::string> codes = table.values();
+
+    int index = codes.indexOf(huffmanCode);
+    // Code found
+    if (index != -1) {
+        return symbols.get(index);
+    
+    // Code not found
+    } else {
+        return std::string();
+    }
 }
 
 template <int treeOrder>
